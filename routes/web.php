63697cd0c3e62c98c174fbd01e4342cd03349c87
NghiaD <dangnghia25197@gmail.com>
2020-02-09 09:20:00 +0700

    build Authorization Server

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('profile', function () {
+    // Only verified users may enter...
+    return "OK";
+})->middleware('verified');
+
+Auth::routes(['verify' => true]);
+Route::get('/home', 'HomeController@index')->name('home');
+
